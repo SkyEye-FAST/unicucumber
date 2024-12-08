dc7d7543bfb0d2eb316a411afd5b6cab4c44b7a9
SkyEye_FAST <skyeyefast@qq.com>
2024-12-09 00:09:16 +0800

    Support PWA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve, dirname } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,39 @@ export default defineConfig({
         dirname(fileURLToPath(import.meta.url)),
         './path/to/src/locales/**',
       ),
+    }),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        'apple-touch-icon.png',
+        'favicon.ico',
+      ],
+      manifest: {
+        name: 'UniCucumber',
+        short_name: 'UniCucumber',
+        display: 'standalone',
+        theme_color: '#4ea72e',
+        description: 'A simple webpage for editing Unifont glyphs in browsers.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+      },
     }),
   ],
   resolve: {
